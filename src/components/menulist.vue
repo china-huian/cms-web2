fdff6d2b9d44c5fc6a62d4cca71cae00fb05d348
litanya <1416520732@qq.com>
2019-05-24 15:10:26 +0800

    1

--- src/components/menulist.vue
+++ src/components/menulist.vue
@@ -1,32 +1,14 @@
 <template>
   <div class="block">
-<<<<<<< HEAD
-    <el-tree
-      class="tree"
-      :data="list"
-      :props="defaultProps"
-      accordion
-      @node-click="handleNodeClick"
-    ></el-tree>
+    <el-tree class="tree" :data="list" :props="defaultProps" accordion @node-click="handleNodeClick"></el-tree>
 
     <div class="content">
-      <el-form
-        :model="listForm"
-        :rules="rules"
-        ref="ruleForm"
-        label-width="100px"
-        class="demo-ruleForm"
-      >
+      <el-form :model="listForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
         <el-form-item label="名称" prop="name">
           <el-input class="input" v-model="listForm.name"></el-input>
         </el-form-item>
         <el-form-item label="类型">
-          <el-select
-            class="fd1"
-            v-model="listForm.type"
-            @change="typequery(listForm.type)"
-            placeholder="请选择内容"
-          >
+          <el-select class="fd1" v-model="listForm.type" @change="typequery(listForm.type)" placeholder="请选择内容">
             <!-- <el-option label="空" value=></el-option> -->
             <el-option label="空" :value="null"></el-option>
             <el-option label="栏目" value="0"></el-option>
@@ -38,12 +20,7 @@
           <el-select class="fd1" v-model="listForm.binding" placeholder="选择绑定的id">
             <el-option label="空" :value="null"></el-option>
             <!-- <el-option label="空" ></el-option> -->
-            <el-option
-              v-for="(item, index) in typelist"
-              :key="index"
-              :label="item.name"
-              :value="item._id"
-            ></el-option>
+            <el-option v-for="(item, index) in typelist" :key="index" :label="item.name" :value="item._id"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="排序" prop="index">
@@ -57,12 +34,7 @@
             <i class="el-icon-edit-outline el-icon--left"></i>
             添加一级菜单
           </el-button>
-          <el-button
-            class="fd1 addbtn"
-            type="success"
-            @click="addTow"
-            v-if="id && children == false"
-          >
+          <el-button class="fd1 addbtn" type="success" @click="addTow" v-if="id && children == false">
             <i class="el-icon-edit-outline el-icon--left"></i>
             新建二级菜单
           </el-button>
@@ -70,131 +42,18 @@
             <i class="el-icon-circle-check el-icon--left"></i>
             创建
           </el-button>
-          <el-button
-            class="fd1 addbtn"
-            type="primary"
-            @click="updates"
-            v-if="id != null && children == false"
-          >
+          <el-button class="fd1 addbtn" type="primary" @click="updates" v-if="id != null && children == false">
             <i class="el-icon-circle-check el-icon--left"></i>
             修改
           </el-button>
-          <el-button
-            class="fd1 addbtn"
-            type="danger"
-            @click="remove"
-            v-if="id != null && children == false"
-          >
+          <el-button class="fd1 addbtn" type="danger" @click="remove" v-if="id != null && children == false">
             <i class="el-icon-delete el-icon--left"></i>
             删除
           </el-button>
-          <el-button
-            class="fd1 addbtn"
-            type="primary"
-            @click="ChildrenAdd"
-            v-if="id && children == true"
-          >
+          <el-button class="fd1 addbtn" type="primary" @click="ChildrenAdd" v-if="id && children == true">
             <i class="el-icon-circle-check el-icon--left"></i>
             创建
           </el-button>
-=======
-    <!-- <el-table :data="data" style="width: 100%" height="70vh">
-      <el-table-column label="菜单名称" width="250">
-        <template slot-scope="scope">
-          <span>{{ scope.row.name }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="创建时间" width="250">
-        <template slot-scope="scope">
-          <span>{{ scope.row.date }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="排序" width="180">
-        <template slot-scope="scope">
-          <span>{{ scope.row.date }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="操作">
-        <template slot-scope="scope">
-          <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-          <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
-        </template>
-      </el-table-column>
-    </el-table>-->
-    <el-tree class="tree" :data="data" :props="defaultProps" accordion @node-click="handleNodeClick"></el-tree>
-    <el-button class="fd1 addbtn" type="success" @click="add">
-      <i class="el-icon-edit-outline  el-icon--left"></i>
-      新建菜单
-    </el-button>
-    <el-button class="fd1 addbtn" type="danger" @click="add">
-      <i class="el-icon-delete  el-icon--left"></i>
-      删除
-    </el-button>
-
-    <div class="content">
-      <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-        <el-form-item label="名称" prop="name">
-          <el-input v-model="ruleForm.name"></el-input>
-        </el-form-item>
-        <el-form-item label="类型" prop="region">
-          <el-select v-model="ruleForm.region" placeholder="请选择内容">
-            <el-option label="栏目" value="lanmu"></el-option>
-            <el-option label="单页" value="danye"></el-option>
-            <el-option label="链接" value="lianjei"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="绑定id" prop="region">
-          <el-select v-model="ruleForm.region" placeholder="请选择内容">
-            <el-option label="菜单内容" value="lanmu"></el-option>
-            <el-option label="菜单内容" value="danye"></el-option>
-            <el-option label="菜单内容" value="lianjei"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="排序" prop="id">
-          <el-input v-model="ruleForm.name"></el-input>
-        </el-form-item>
-        <!-- <el-form-item label="时间" required>
-          <el-col :span="11">
-            <el-form-item prop="date1">
-              <el-date-picker
-                type="date"
-                placeholder="选择日期"
-                v-model="ruleForm.date1"
-                style="width: 100%;"
-              ></el-date-picker>
-            </el-form-item>
-          </el-col>
-          <el-col class="line" :span="2">-</el-col>
-          <el-col :span="11">
-            <el-form-item prop="date2">
-              <el-time-picker placeholder="选择时间" v-model="ruleForm.date2" style="width: 100%;"></el-time-picker>
-            </el-form-item>
-          </el-col>
-        </el-form-item> -->
-        <!-- <el-form-item label="即时配送" prop="delivery">
-          <el-switch v-model="ruleForm.delivery"></el-switch>
-        </el-form-item> -->
-        <!-- <el-form-item label="活动性质" prop="type">
-          <el-checkbox-group v-model="ruleForm.type">
-            <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-            <el-checkbox label="地推活动" name="type"></el-checkbox>
-            <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-            <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-          </el-checkbox-group>
-        </el-form-item> -->
-        <!-- <el-form-item label="特殊资源" prop="resource">
-          <el-radio-group v-model="ruleForm.resource">
-            <el-radio label="线上品牌商赞助"></el-radio>
-            <el-radio label="线下场地免费"></el-radio>
-          </el-radio-group>
-        </el-form-item> -->
-        <!-- <el-form-item label="活动形式" prop="desc">
-          <el-input type="textarea" v-model="ruleForm.desc"></el-input>
-        </el-form-item> -->
-        <el-form-item>
-          <el-button type="primary" @click="submitForm('ruleForm')">创建</el-button>
-          <el-button @click="resetForm('ruleForm')">重置</el-button>
->>>>>>> Riley
         </el-form-item>
       </el-form>
     </div>
@@ -202,7 +61,6 @@
 </template>
 
 <script>
-<<<<<<< HEAD
 import { createNamespacedHelpers } from 'vuex';
 // import { constants } from 'crypto';
 const { mapActions: pageActions } = createNamespacedHelpers('page');
@@ -239,76 +97,12 @@ export default {
     ...pageActions({ queryPage: 'query' }),
     ...columnActions({ queryColumn: 'query' }),
     ...mapActions(['query', 'add', 'delete', 'update', 'fetch']),
-=======
-import { mapActions } from 'vuex';
-export default {
-  data() {
-    return {
-      data: [
-        {
-          // name: '菜单管理',
-          // date: '2019-04-25',
-          label: '一级 1',
-          children: [
-            {
-              label: '二级 1-1',
-            },
-          ],
-        },
-        {
-          label: '一级 2',
-          children: [
-            {
-              label: '二级 2-1',
-            },
-            {
-              label: '二级 2-2',
-            },
-          ],
-        },
-        {
-          label: '一级 3',
-          children: [
-            {
-              label: '二级 3-1',
-            },
-            {
-              label: '二级 3-2',
-            },
-          ],
-        },
-      ],
-      defaultProps: {
-        children: 'children',
-        label: 'label',
-      },
-      ruleForm: {
-        name: '',
-        id: '',
-        region: '',
-        date1: '',
-        date2: '',
-        // delivery: false,
-      },
-      rules: {
-        name: [{ required: true, message: '请输入名称', trigger: 'blur' }, { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }],
-        id: [{ required: true, message: '请输入id', trigger: 'blur' }, { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }],
-        region: [{ required: true, message: '请选择类型', trigger: 'change' }],
-        date1: [{ type: 'date', required: true, message: '请选择日期', trigger: 'change' }],
-        date2: [{ type: 'date', required: true, message: '请选择时间', trigger: 'change' }],
-      },
-    };
-  },
-  methods: {
-    ...mapActions('menu', ['query']),
->>>>>>> Riley
     open(msg) {
       this.$message({
         message: msg,
         type: 'success',
       });
     },
-<<<<<<< HEAD
     async handleNodeClick(datas, a) {
       this.children = false;
       if (a.parent.data._id) {
@@ -385,21 +179,6 @@ export default {
       } else {
         this.$message.error('请完整填写带有红色星号的必填内容');
       }
-=======
-    handleNodeClick() {},
-    submitForm(formName) {
-      this.$refs[formName].validate(valid => {
-        if (valid) {
-          alert('submit!');
-        } else {
-          console.log('error submit!!');
-          return false;
-        }
-      });
-    },
-    resetForm(formName) {
-      this.$refs[formName].resetFields();
->>>>>>> Riley
     },
     // 删除
     async remove() {
@@ -479,10 +258,7 @@ export default {
 .tree {
   /* background-color: #123456; */
   float: left;
-<<<<<<< HEAD
   margin-top: 1em;
-=======
->>>>>>> Riley
   width: 20%;
 }
 .content {
@@ -490,12 +266,9 @@ export default {
   width: 70%;
   margin: 1em;
   /* background-color: #b52415; */
-<<<<<<< HEAD
 }
 .input {
   width: 50%;
   float: left;
-=======
->>>>>>> Riley
 }
 </style>
